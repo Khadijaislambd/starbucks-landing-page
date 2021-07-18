497f24fb9cce8738b9b9def77ee8788b4aac1665
Khadijaislambd <contact.khadijabegum@gmail.com>
2021-07-18 07:34:38 -0700

    file send

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+ function imgSlider(anything) {
+     document.querySelector('.starbucks').src = anything;
+ }
+function changeCircleColor(color){
+ 
+    const circle = document.querySelector('.circle');
+    circle.style.backgroud = color;
+}
+
+
+
+
+ 
+
+// var myVar = document.querySelector("#myImageId");
+// function picture1(){
+//     myVar.src = "img/red.png";
+// }
+// function picture2(){
+//     myVar.src = "img/green.png";
+// }
+// function picture3(){
+//     myVar.src = "img/dred.png";
+// }
+// function picture4(){
+//     myVar.src = "img/sky.png";
+// }
